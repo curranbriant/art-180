3f49b2200dc53d4b01784447a32d7f439f352914
Brian Curran <curranbriant@Brians-MacBook-Pro.local>
2019-09-20 17:32:12 -0400

    adds callback page

---
+++ callback.js
@@ -0,0 +1,6 @@
+//If successful, Square Point of Sale returns the following parameters.
+const clientTransactionId = "client_transaction_id";
+const transactionId = "transaction_id";
+
+//If there's an error, Square Point of Sale returns the following parameters.
+const errorField = "error_code";
