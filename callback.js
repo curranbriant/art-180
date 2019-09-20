d09aca7b8b7691d19990e8f5b0edf25ffbd51eb9
Brian Curran <curranbriant@Brians-MacBook-Pro.local>
2019-09-20 17:38:41 -0400

    adds to callback

--- callback.js
+++ callback.js
@@ -4,3 +4,60 @@ const transactionId = "transaction_id";
 
 //If there's an error, Square Point of Sale returns the following parameters.
 const errorField = "error_code";
+
+function getTransactionInfo(URL) {
+    var data = decodeURI(URL.searchParams.get("data"));
+
+    console.log("data: " + data);
+    var transactionInfo = JSON.parse(data);
+    return transactionInfo;
+  }
+  // Makes a result string for success situation
+function handleSuccess(transactionInfo){
+    var resultString ="";
+  
+    if (clientTransactionId in transactionInfo) {
+      resultString += "Client Transaction ID: " + transactionInfo[clientTransactionId] + "<br>";
+    }
+    if (transactionId in transactionInfo) {
+      resultString += "Transaction ID: " + transactionInfo[transactionId] + "<br>";
+    }
+     else {
+      resultString += "Transaction ID: NO CARD USED<br>";
+    }
+    return resultString;
+  }
+  
+  
+  // Makes an error string for error situation
+  function handleError(transactionInfo){
+    var resultString ="";
+  
+    if (errorField in transactionInfo) {
+      resultString += "Client Transaction ID: " + transactionInfo[clientTransactionId] + "<br>";
+    }
+    if (transactionId in transactionInfo) {
+      resultString += "Transaction ID: " + transactionInfo[transactionId] + "<br>";
+    }
+     else {
+      resultString += "Transaction ID: PROCESSED OFFLINE OR NO CARD USED<br>";
+    }
+    return resultString;
+  }
+  
+  
+
+  function printResponse() {
+    var responseUrl = window.location.href;
+    var transactionInfo = getTransactionInfo(responseUrl);
+    var resultString = "";
+  
+    if (errorField in transactionInfo) {
+      resultString = handleError(transactionInfo);
+    } else {
+      resultString = handleSuccess(transactionInfo);
+    }
+  
+    document.getElementById('url').innerHTML = resultString;
+  }
+  
